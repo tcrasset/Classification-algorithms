f69e58a27437e4328b6aa0c4414c93ccb7ad72b2
arcticdreamz <tomcrasset@gmail.com>
2018-10-29 17:04:30 +0100

    Create trainEstimator function

--- dt.py
+++ dt.py
@@ -19,43 +19,35 @@ from plot import plot_boundary
 # Put your funtions here
 # ...
 
+def trainEstimator(nbGen, maxdepth):
+    for generation in range(nbGen):
+        nbPoints = 1500
+        seed = generation
+        X, y = make_dataset2(nbPoints, seed)
+        accuracy_arr = np.zeros(nbGen)
+
+        X_ls, X_ts, y_ls, y_ts = train_test_split(X, y, train_size = 1200, test_size = 300)
+
+        if maxdepth == "None":
+            estimator = DecisionTreeClassifier().fit(X_ls,y_ls)
+        else:
+            estimator = DecisionTreeClassifier(max_depth=maxdepth).fit(X_ls,y_ls)
+
+        y_pred = estimator.predict(X_ts)
+
+        if generation == 1:
+            plot_boundary("DT maxdepth {}".format(maxdepth),estimator , X_ts, y_ts, 0.1)
+        
+        accuracy_arr[generation] = accuracy_score(y_ts, y_pred)
+    return accuracy_arr
+
 
 if __name__ == "__main__":
-    for generation in range(1):
-        nbPoints = 1500
-        X, y = make_dataset2(nbPoints)
-
-
-        numberIterations = 15
-        accuracy_arr = np.empty((numberIterations,5))
-        
-        for i in range(0,numberIterations):
-            X_ls, X_ts, y_ls, y_ts = train_test_split(X, y , train_size = 1200/1500)
-            
-            estimator_maxdepth_1 = DecisionTreeClassifier(max_depth=1).fit(X_ls,y_ls)
-            estimator_maxdepth_2 = DecisionTreeClassifier(max_depth=2).fit(X_ls,y_ls)
-            estimator_maxdepth_4 = DecisionTreeClassifier(max_depth=4).fit(X_ls,y_ls)
-            estimator_maxdepth_8 = DecisionTreeClassifier(max_depth=8).fit(X_ls,y_ls)
-            estimator_maxdepth_None = DecisionTreeClassifier().fit(X_ls,y_ls)    
-
-            estimators = [
-                estimator_maxdepth_1,
-                estimator_maxdepth_2,
-                estimator_maxdepth_4,
-                estimator_maxdepth_8,
-                estimator_maxdepth_None
-            ]
-            for estimator, j in zip(estimators,range(5)):
-                y_pred = estimator.predict(X_ts)
-                accuracy_arr[i][j] = accuracy_score(y_ts, y_pred)
-
-        accuracy_arr = accuracy_arr.T #transpose array
-
-        labels = [1,2,4,8,"None"]
-        print("Generation {}".format(generation))
-        print("Number of iterations: {}".format(numberIterations))
-        print("Estimator \t Mean \t STD")
-
-        for label, i in zip(labels,range(5)):
-            print("{:>10} \t {:.3f} \t {:.4f}".format(label,accuracy_arr[i].mean(),accuracy_arr[i].std()))
-            plot_boundary("DT maxdepth {}".format(label),estimators[i], X_ts, y_ts,0.1)
+    nbGen = 5
+    maxdepths = [1, 2, 4, 8, "None"]
+    
+    for maxdepth in maxdepths:
+        print("Maximal depth : {}".format(maxdepth))
+        print("Mean \t STD")
+        accuracy = trainEstimator(5, maxdepth)
+        print("{:.3f} \t {:.4f}".format(accuracy.mean(), accuracy.std()))
