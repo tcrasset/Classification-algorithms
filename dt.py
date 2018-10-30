4c2cd4d8b4bceb37685380326de48c27ab0f0c58
“Antoine <antoine.louis02@outlook.fr>
2018-10-30 16:20:27 +0100

    Add comments and change function and variables names

--- dt.py
+++ dt.py
@@ -8,45 +8,54 @@ Project 1 - Classification algorithms
 
 import numpy as np
 from matplotlib import pyplot as plt
-
 from data import make_dataset1, make_dataset2
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from plot import plot_boundary
-# (Question 1)
 
-# Put your funtions here
-# ...
 
-def trainEstimator(nbGen, maxdepth):
-    accuracy_arr = []
-    for generation in range(nbGen):
-        nbPoints = 1500
+
+def compute_accuracy(nb_gen, max_depth):
+    """Computes the test set accurencies over n generations of the dataset
+    for a particular depth.
+
+        Parameters
+        ----------
+        -   nb_gen : number of generations of the dataset
+        -   max_depth : maximum depth of the decision tree
+
+        Returns
+        -------
+        accuracy : a list of the test set accuracies of the different generations
+    """
+    accuracy = []
+    nb_points = 1500
+
+    for generation in range(nb_gen):
         seed = generation
-        X, y = make_dataset2(nbPoints, seed)
-
-        X_ls, X_ts, y_ls, y_ts = train_test_split(X, y, train_size = 1200, test_size = 300)
+        X, y = make_dataset2(nb_points, seed)
+        X_ls, X_ts, y_ls, y_ts = train_test_split(X, y, train_size=.8)
         
-        if maxdepth == "None":
-            estimator = DecisionTreeClassifier().fit(X_ls,y_ls)
+        if max_depth == "None":
+            estimator = DecisionTreeClassifier().fit(X_ls, y_ls)
         else:
-            estimator = DecisionTreeClassifier(max_depth=maxdepth).fit(X_ls,y_ls)
+            estimator = DecisionTreeClassifier(max_depth=max_depth).fit(X_ls, y_ls)
 
         y_pred = estimator.predict(X_ts)
-        accuracy_arr.append(accuracy_score(y_ts, y_pred))
+        accuracy.append(accuracy_score(y_ts, y_pred))
 
         if generation == 1:
-            plot_boundary("DT maxdepth {}".format(maxdepth),estimator , X_ts, y_ts, 0.1)
-    return accuracy_arr
+            plot_boundary("DT max_depth {}".format(max_depth), estimator , X_ts, y_ts, 0.1)
 
+    return accuracy
 
 if __name__ == "__main__":
-    nbGen = 5
-    maxdepths = [1, 2, 4, 8, "None"]
+    nb_gen = 5
+    max_depths = [1, 2, 4, 8, "None"]
     
-    for maxdepth in maxdepths:
-        print("Maximal depth : {}".format(maxdepth))
+    for max_depth in max_depths:
+        print("Maximal depth : {}".format(max_depth))
         print("Mean \t STD")
-        accuracy = np.array(trainEstimator(nbGen, maxdepth))
+        accuracy = np.array(compute_accuracy(nb_gen, max_depth))
         print("{:.3f} \t {:.4f}".format(accuracy.mean(), accuracy.std()))
