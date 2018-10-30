8379f6168f2e7bae015b98f2f933724fa42ea7e3
arcticdreamz <tomcrasset@gmail.com>
2018-10-30 12:17:44 +0100

    Change pi and mu to dictionnaries &compute f_k(x)

--- lda.py
+++ lda.py
@@ -40,51 +40,63 @@ class LinearDiscriminantAnalysis(BaseEstimator, ClassifierMixin):
         if y.shape[0] != X.shape[0]:
             raise ValueError("The number of samples differs between X and y")
 
+        #Probability of belonging to given class
+
+        # Shape :
+        # pi = {class1 : probability,
+        #       class2 : probability,
+        #        ...
+        #      }
+        pi = {}
+
         # Elementwise mean of attributes per class 
         # Shape :
-        # pi = [[key, probability],
-        #       [key, probability],
+        # mu = {class1 : [mean of attributes],
+        #       class2 : [mean of attributes],
         #        ...
-        #      ]
-        pi = []
-        #Probability of belonging to given class
-        # Shape :
-        # mu = [[key, [mean of attributes]],
-        #       [key, [mean of attributes]],
-        #        ...
-        #      ]
-        mu = []
+        #      }
+        mu = {}
+
         X_norm = []
         Sigma = None
+
         dictionary = {}
         # Stores for each class k a tuple 
-        # (nb_occurance of class k, sum of attributes of all samples belonging to k)
+        # (nb_occurance of class k, list of all samples belonging to k)
         # dictionnary[k][0] =  nb_occurance
-        # dictionnary[k][1] =  sum of attributes of all samples belonging to k
+        # dictionnary[k][1] =  list of all samples belonging to k
         dictionary = {}
 
+        #Separating the points into their class
         for _X, _y in zip(X,y):
             if _y in dictionary:
-                dictionary[_y][0] += 1
+                dictionary[_y][0] += 1 
                 dictionary[_y][1].append(_X)
             else:
                 dictionary[_y] = [1, [_X] ]
 
+        #Computing pi and mu
         for key, value in zip(dictionary.keys(), dictionary.values()):
             attributes_array = np.array(value[1])
-            mu.append([key, np.mean(attributes_array,axis=0)])
-            pi.append([key, value[0]/y.shape[0]])
+            mu[key] = np.mean(attributes_array,axis=0) # Elementwise mean of attributes per class
+            pi[key] = value[0]/y.shape[0] # Probability of belonging to class
             #Normalize attributes with the mean of their class
-            X_norm.append(attributes_array - mu[-1][1])
+            X_norm.append(attributes_array - mu[key])
 
         #Concatenating all the points into one array again
-        X_norm[1] = np.array(X_norm[1])
         X_norm[0] = np.array(X_norm[0])
+        X_norm[1] = np.array(X_norm[1])
         X_norm = np.concatenate((X_norm[0], X_norm[1]), axis=0)
 
         #Covariance matrix
         Sigma = np.cov(X_norm, rowvar=False)
 
+        const = 1/(2*math.pi**(X.shape[1]/2)*np.sqrt(np.abs(Sigma)))
+        for _x, _y in zip(X,y):
+            _xnorm = _x - mu[_y]
+            exposant = math.exp(-0.5 * np.transpose(_xnorm)@np.linalg.inv(Sigma)@_xnorm)
+            function = const*exposant
+            
         return self
 
     def predict(self, X):
